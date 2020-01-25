931f2f9343f1390993efc65f92387925ce18e19b
John Melody Me, Tan Sin Dee <Johnmelodyme@yandex.com>
2020-01-26 01:12:52 +0800

    Uploaded

---
+++ app/src/main/java/com/johnmelodyme/speechtotextengine/MainActivity.java
@@ -0,0 +1,19 @@
+package com.johnmelodyme.speechtotextengine;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @Author : John Melody Melissa
+ * @Inspired_by : {GF} Sin Dee 😍🥰🥰
+ * @Project_name: Speech To Text
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
